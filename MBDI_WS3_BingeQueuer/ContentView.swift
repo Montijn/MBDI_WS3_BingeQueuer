a58ef5fdb885ad555846ccb0e099e5e1bfbc0db8
Montijn <ms.bennik@student.avans.nl>
2023-03-14 11:44:17 +0100

    Initial Commit

---
+++ MBDI_WS3_BingeQueuer/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MBDI_WS3_BingeQueuer
+//
+//  Created by Montijn Bennik on 14/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MBDI_WS3_BingeQueuer/MBDI_WS3_BingeQueuerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MBDI_WS3_BingeQueuerApp.swift
+//  MBDI_WS3_BingeQueuer
+//
+//  Created by Montijn Bennik on 14/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MBDI_WS3_BingeQueuerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
